8a8a5f449d741dd4926e6cfa63de178153fa9288
Pranav Nutalapati <pranavnutalapati@gmail.com>
2024-10-20 23:23:27 -0400

    yooo we made it

--- twl-clone.c
+++ twl-clone.c
@@ -101,6 +101,9 @@ struct twl_clone_keyboard {
 
 	struct twl_clone_server *server;
 	struct wlr_keyboard *wlr_keyboard;
+	struct wl_listener modifiers;
+	struct wl_listener key;
+	struct wl_listener destroy;
 };
 
 static struct twl_clone_toplevel *desktop_toplevel_at(
@@ -405,6 +408,71 @@ static void server_cursor_frame(struct wl_listener *listener, void *data) {
 }
 
 
+static void keyboard_handle_modifiers(
+		struct wl_listener *listener, void *data) {
+	struct twl_clone_keyboard *keyboard = wl_container_of(listener, keyboard, modifiers);
+	// oh this is wack. wayland only lets you have one keyboard per seat
+	// so the tech is to... keep swapping... them...
+	wlr_seat_set_keyboard(keyboard->server->seat, keyboard->wlr_keyboard);
+	wlr_seat_keyboard_notify_modifiers(keyboard->server->seat,
+		&keyboard->wlr_keyboard->modifiers);
+}
+
+static bool handle_keybinding(struct twl_clone_server *server, xkb_keysym_t sym) {
+	switch (sym) {
+	case XKB_KEY_Escape:
+		wl_display_terminate(server->wl_display);
+		break;
+	case XKB_KEY_F1:
+		if (wl_list_length(&server->toplevels) < 2) {
+			break;
+		}
+		struct twl_clone_toplevel *next_toplevel = 
+			wl_container_of(server->toplevels.prev, next_toplevel, link);
+		focus_toplevel(next_toplevel, next_toplevel->xdg_toplevel->base->surface);
+		break;
+	default:
+		return false;
+	}
+	return true;
+}
+
+
+static void keyboard_handle_key(struct wl_listener *listener, void *data) {
+	struct twl_clone_keyboard *keyboard = wl_container_of(listener, keyboard, key);
+	struct twl_clone_server *server = keyboard->server;
+	struct wlr_keyboard_key_event *event = data;
+	struct wlr_seat *seat = server->seat;
+
+	// libinput keycode -> xkbcommon
+	uint32_t keycode = event->keycode + 8;
+	const xkb_keysym_t *syms;
+	int nsyms = xkb_state_key_get_syms(
+			keyboard->wlr_keyboard->xkb_state, keycode, &syms);
+
+	bool handled = false;
+	uint32_t modifiers = wlr_keyboard_get_modifiers(keyboard->wlr_keyboard);
+	if ((modifiers & WLR_MODIFIER_ALT) && event->state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+		for (int i = 0; i < nsyms; i++) {
+			handled = handle_keybinding(server, syms[i]);
+		}
+	}
+	if (!handled) {
+		wlr_seat_set_keyboard(seat, keyboard->wlr_keyboard);
+		wlr_seat_keyboard_notify_key(seat, event->time_msec, 
+			event->keycode, event->state);
+	}
+}
+
+static void keyboard_handle_destroy(struct wl_listener *listener, void *data) {
+	struct twl_clone_keyboard *keyboard = wl_container_of(listener, keyboard, destroy); 
+	wl_list_remove(&keyboard->modifiers.link);
+	wl_list_remove(&keyboard->key.link);
+	wl_list_remove(&keyboard->destroy.link);
+	wl_list_remove(&keyboard->link);
+	free(keyboard);
+}
+
 static void server_new_keyboard(struct twl_clone_server *server, struct wlr_input_device *device) {
 	wlr_log(WLR_INFO, "new input is a keyboard!");
 	struct wlr_keyboard *wlr_keyboard = wlr_keyboard_from_input_device(device);
@@ -413,7 +481,23 @@ static void server_new_keyboard(struct twl_clone_server *server, struct wlr_inpu
 	keyboard->server = server;
 	keyboard->wlr_keyboard = wlr_keyboard;
 
-	// TODO: setup keyboard keymap and events
+	// set up the keymap
+	struct xkb_context *context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+	struct xkb_keymap *keymap = xkb_keymap_new_from_names(context, NULL,
+			XKB_KEYMAP_COMPILE_NO_FLAGS);
+
+	wlr_keyboard_set_keymap(wlr_keyboard, keymap);
+	xkb_keymap_unref(keymap);
+	xkb_context_unref(context);
+	wlr_keyboard_set_repeat_info(wlr_keyboard, 25, 600);
+
+	// add event listeners
+	keyboard->modifiers.notify = keyboard_handle_modifiers;
+	wl_signal_add(&wlr_keyboard->events.modifiers, &keyboard->modifiers);
+	keyboard->key.notify = keyboard_handle_key;
+	wl_signal_add(&wlr_keyboard->events.key, &keyboard->key);
+	keyboard->destroy.notify = keyboard_handle_destroy;
+	wl_signal_add(&device->events.destroy, &keyboard->destroy);
 	
 	wlr_seat_set_keyboard(server->seat, keyboard->wlr_keyboard);
 
@@ -695,7 +779,7 @@ int main(int argc, char *argv[]) {
 	server.scene = wlr_scene_create();
 	server.scene_layout = wlr_scene_attach_output_layout(server.scene, server.output_layout);
 
-	// TODO: Initialize xdg-shellv3 (used for application windows)
+	// Initialize xdg-shellv3 (used for application windows)
 	wl_list_init(&server.toplevels);
 	server.xdg_shell = wlr_xdg_shell_create(server.wl_display, 3);
 	server.new_xdg_toplevel.notify = server_new_xdg_toplevel;
@@ -704,7 +788,7 @@ int main(int argc, char *argv[]) {
 	wl_signal_add(&server.xdg_shell->events.new_popup, &server.new_xdg_popup);
 	
 
-	// TODO: Add cursor support in a way that doesnt segfault pls and thank 
+	// Add cursor support in a way that doesnt segfault pls and thank 
 	
 	server.cursor = wlr_cursor_create();
 	wlr_cursor_attach_output_layout(server.cursor, server.output_layout);
